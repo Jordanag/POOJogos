e7bc236d9fc226f0809e93c06858ebbdb42b15bc
Jordana <jordanagomes51@gmail.com>
2016-06-21 08:22:37 -0300

    hello wolrd

---
+++ hello wolrd.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //diretiva do pre-processador e deve ser escrita em uma unica linha
+using namespace std;//agrega um identificador a todos os nomes declarados dentro dela
+
+int main() // função principal do programa. marca o ponto de partida
+{ //abrindo função principal.
+	
+	cout << "HelloWorld!" << endl; //imprime a mensagem entre aspas na tela
+	return 0; //instrução de retorno
+} //fechando função principal.
